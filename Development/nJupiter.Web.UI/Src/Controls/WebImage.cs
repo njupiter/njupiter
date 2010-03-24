6d9a880fd2700a4df83eef42584ef72ab173f38c
martin.odhelius <martin.odhelius@81644cf2-1715-11df-9fa9-695f62f9af20>
2010-03-24 15:09:52 +0000

    Fixed non compiling code committed by deleeuw

--- Development/nJupiter.Web.UI/Src/Controls/WebImage.cs
+++ Development/nJupiter.Web.UI/Src/Controls/WebImage.cs
@@ -229,9 +229,9 @@ namespace nJupiter.Web.UI.Controls {
 
 		public virtual string RenderImageUrl() {
 			if(this.MaxWidth > 0 || this.MaxHeight > 0 || this.ForceStreaming){
-				string streamingPath = Creuna.Util.Web.UrlHandler.AddQueryParams(this.StreamingPath, "path=" + System.Web.HttpUtility.UrlEncode(this.ImageUrl));
+				string streamingPath = nJupiter.Web.UrlHandler.AddQueryParams(this.StreamingPath, "path=" + System.Web.HttpUtility.UrlEncode(this.ImageUrl));
 				if(this.MaxWidth > 0 || this.MaxHeight > 0) {
-					streamingPath = Creuna.Util.Web.UrlHandler.AddQueryParams(streamingPath, "width=" + this.MaxWidth, "height=" + this.MaxHeight, "allowEnlarging=" + this.AllowEnlarging, "allowStretching=" + this.AllowStretching);
+					streamingPath = nJupiter.Web.UrlHandler.AddQueryParams(streamingPath, "width=" + this.MaxWidth, "height=" + this.MaxHeight, "allowEnlarging=" + this.AllowEnlarging, "allowStretching=" + this.AllowStretching);
 				}
 				return streamingPath;
 			}
