ddbc6aa2143fd8d66a97f2685de1e69dad1dd64a
martin.odhelius <martin.odhelius@81644cf2-1715-11df-9fa9-695f62f9af20>
2010-03-31 09:47:53 +0000

    Fixing nightly build and build so they return correct error codes if something goes wrong.

--- Development/nJupiter.BuildNightly.bat
+++ Development/nJupiter.BuildNightly.bat
@@ -1,6 +1,7 @@
 
 echo Build, debug only
 call nJupiter.BuildAll.bat Debug
+if %ERRORLEVEL% NEQ 0 goto builderror
 
 REM Cd back to current directory
 CD /d %~dp0
@@ -12,3 +13,9 @@ echo Copy build to nightly release directory
 for /F "tokens=2 delims== " %%i in (revision.tmp) do xcopy /Y /E ..\Build C:\Releases\nJupiter\Nightly\%%i\
 del revision.tmp
 
+goto end
+
+:builderror
+echo Build nightly failed
+exit /B 1
+:end
