924cfa585222e84d23c769eda8f9a3e32d45a72a
deleeuw@gmail.com <deleeuw@gmail.com@81644cf2-1715-11df-9fa9-695f62f9af20>
2010-07-06 16:46:31 +0000

    Added the AutoHyperlinkHtml method, that does the same thing as AutoHyperlinkText but ignores URLs within tags (<a href="[url]">) and URLs between the start and end tags for the anchor tag (<a>[url]</a>).

--- Development/nJupiter.Web.UI/Src/HtmlHandler.cs
+++ Development/nJupiter.Web.UI/Src/HtmlHandler.cs
@@ -36,19 +36,23 @@ namespace nJupiter.Web.UI {
 
 	public static class HtmlHandler {
 		#region Constants
-		private const string Colon = ":";
-		private const string FormatReplaceUrl = "<" + HtmlTag.A + " {0}{1}" + HtmlAttribute.Href + @"=""{2}"">{3}</" + HtmlTag.A + ">";
-		private const string NoFollowAttribute = HtmlAttribute.Rel + "=\"external nofollow\" ";
-		private const string FormatAttribute = " {0}=\"{1}\" ";
-		private const string RegexpatternInformalurlPrefix = @"\w+:";
-		private const string RegexpatternInformalurlEmail = @"((?>[a-zA-Z\d!#$%&'*+\-/=?^_`{|}~]+\x20*|""((?=[\x01-\x7f])[^""\\]|\\[\x01-\x7f])*""\x20*)*(?<angle><))?((?!\.)(?>\.?[a-zA-Z\d!#$%&'*+\-/=?^_`{|}~]+)+|""((?=[\x01-\x7f])[^""\\]|\\[\x01-\x7f])*"")@(((?!-)[a-zA-Z\d\-]+(?<!-)\.)+[a-zA-Z]{2,}|\[(((?(?<!\[)\.)(25[0-5]|2[0-4]\d|[01]?\d?\d)){4}|[a-zA-Z\d\-]*[a-zA-Z\d]:((?=[\x01-\x7f])[^\\\[\]]|\\[\x01-\x7f])+)\])(?(angle)>)";
-		private const string RegexpatternInformalurl = RegexpatternInformalurlEmail + "|((" + RegexpatternInformalurlPrefix + @"//|[nN][eE][wW][sS]:|[mM][aA][iI][lL][tT][oO]:|[wW][wW][wW]\.|[fF][tT][pP]\.)[^\\{}|[\]^<>""'\s]*[^\\{}|[\]^<>""'\s.,;?:!)])";
+		private const string	Colon									= ":";
+		private const string	FormatReplaceUrl						= "<" + HtmlTag.A + " {0}{1}" + HtmlAttribute.Href + @"=""{2}"">{3}</" + HtmlTag.A + ">";
+		private const string	NoFollowAttribute						= HtmlAttribute.Rel + "=\"external nofollow\" ";
+		private const string	FormatAttribute							= " {0}=\"{1}\" ";
+		private const string	RegexpatternInformalurlPrefix			= @"\w+:";
+		private const string	RegexpatternInformalurlEmail			= @"((?>[a-zA-Z\d!#$%&'*+\-/=?^_`{|}~]+\x20*|""((?=[\x01-\x7f])[^""\\]|\\[\x01-\x7f])*""\x20*)*(?<angle><))?((?!\.)(?>\.?[a-zA-Z\d!#$%&'*+\-/=?^_`{|}~]+)+|""((?=[\x01-\x7f])[^""\\]|\\[\x01-\x7f])*"")@(((?!-)[a-zA-Z\d\-]+(?<!-)\.)+[a-zA-Z]{2,}|\[(((?(?<!\[)\.)(25[0-5]|2[0-4]\d|[01]?\d?\d)){4}|[a-zA-Z\d\-]*[a-zA-Z\d]:((?=[\x01-\x7f])[^\\\[\]]|\\[\x01-\x7f])+)\])(?(angle)>)";
+		private const string	RegexpatternInformalurl					= RegexpatternInformalurlEmail + "|((" + RegexpatternInformalurlPrefix + @"//|[nN][eE][wW][sS]:|[mM][aA][iI][lL][tT][oO]:|[wW][wW][wW]\.|[fF][tT][pP]\.)[^\\{}|[\]^<>""'\s]*[^\\{}|[\]^<>""'\s.,;?:!)])";
+		private const string 	RegexpatternFullATagOrTag				= @"(</?[aA]+((\s+\w+(\s*=\s*(?:"".*?""|'.*?'|[^'"">\s]+))?)+\s*|\s*)/?>.*?</[aA]>)|(</?\w+((\s+\w+(\s*=\s*(?:"".*?""|'.*?'|[^'"">\s]+))?)+\s*|\s*)/?>)";
+		private const string	RegexpatternFullATagOrTagOrInformalUrl	= "(" + RegexpatternFullATagOrTag + ")|(" + RegexpatternInformalurl + ")";
 		#endregion
 
 		#region Members
-		private static readonly Regex InformalUrlRegex = new Regex(RegexpatternInformalurl, RegexOptions.ExplicitCapture | RegexOptions.Compiled);
-		private static readonly Regex InformalUrlEmailRegex = new Regex(RegexpatternInformalurlEmail, RegexOptions.ExplicitCapture | RegexOptions.Compiled);
-		private static readonly Regex InformalUrlPrefixRegex = new Regex(RegexpatternInformalurlPrefix, RegexOptions.ExplicitCapture | RegexOptions.Compiled);
+		private static readonly Regex	InformalUrlRegex				= new Regex(RegexpatternInformalurl, RegexOptions.ExplicitCapture | RegexOptions.Compiled);
+		private static readonly Regex	InformalUrlEmailRegex			= new Regex(RegexpatternInformalurlEmail, RegexOptions.ExplicitCapture | RegexOptions.Compiled);
+		private static readonly Regex	InformalUrlPrefixRegex			= new Regex(RegexpatternInformalurlPrefix, RegexOptions.ExplicitCapture | RegexOptions.Compiled);
+		private static readonly Regex	FullATagOrTagRegex				= new Regex(RegexpatternFullATagOrTag, RegexOptions.ExplicitCapture | RegexOptions.Compiled);
+		private static readonly Regex	FullATagOrTagOrInformalUrlRegex = new Regex(RegexpatternFullATagOrTagOrInformalUrl, RegexOptions.ExplicitCapture | RegexOptions.Compiled);
 		#endregion
 
 		#region Methods
@@ -61,15 +65,15 @@ namespace nJupiter.Web.UI {
 			for(int i = 0; i < text.Length; i++) {
 				switch(text[i]) {
 					case '<':
-					beginRemove = i;
-					break;
+						beginRemove = i;
+						break;
 					case '>':
-					if(!beginRemove.Equals(-1)) {
-						int length = i - beginRemove + 1;
-						text.Remove(beginRemove, length);
-						i -= length;
-					}
-					beginRemove = -1;
+						if(!beginRemove.Equals(-1)) {
+							int length = i - beginRemove + 1;
+							text.Remove(beginRemove, length);
+							i -= length;
+						}
+						beginRemove = -1;
 					break;
 				}
 			}
@@ -86,35 +90,24 @@ namespace nJupiter.Web.UI {
 		}
 		public static string AutoHyperlinkText(string text, bool noFollow, HtmlLinkEvaluator htmlLinkEvaluator) {
 			return InformalUrlRegex.Replace(text, delegate(Match match) {
-				string linkText = match.Value;
-				string linkUrl;
-				StringBuilder linkAttributes = null;
-				if(InformalUrlPrefixRegex.IsMatch(linkText)) {
-					linkUrl = linkText;
-				} else if(InformalUrlEmailRegex.IsMatch(linkText)) {
-					linkUrl = Uri.UriSchemeMailto + Colon + linkText;
-				} else if(string.Compare(linkText.Substring(0, 3), Uri.UriSchemeFtp, true, CultureInfo.InvariantCulture).Equals(0)) {
-					linkUrl = Uri.UriSchemeFtp + Uri.SchemeDelimiter + linkText;
-				} else {
-					linkUrl = Uri.UriSchemeHttp + Uri.SchemeDelimiter + linkText;
-				}
-				if(htmlLinkEvaluator != null) {
-					HtmlLink htmlLink = htmlLinkEvaluator(new HtmlLink(linkUrl, linkText));
-					linkUrl = htmlLink.Url;
-					linkText = htmlLink.Text;
-					if(!htmlLink.Attributes.Count.Equals(0)) {
-						linkAttributes = new StringBuilder();
-						foreach(string attribute in htmlLink.Attributes.Keys) {
-							linkAttributes.AppendFormat(CultureInfo.InvariantCulture, FormatAttribute, attribute, htmlLink.Attributes[attribute]);
-						}
-					}
-				}
-				return string.Format(CultureInfo.InvariantCulture, 
-					FormatReplaceUrl, 
-					noFollow ? NoFollowAttribute : string.Empty, 
-					linkAttributes,
-					linkUrl, 
-					linkText);
+				return HyperlinkUrl(match.Value, noFollow, htmlLinkEvaluator);
+			});
+		}
+		public static string AutoHyperlinkHtml(string html) {
+			return AutoHyperlinkHtml(html, false, null);
+		}
+		public static string AutoHyperlinkHtml(string html, bool noFollow) {
+			return AutoHyperlinkHtml(html, noFollow, null);
+		}
+		public static string AutoHyperlinkHtml(string html, HtmlLinkEvaluator htmlLinkEvaluator) {
+			return AutoHyperlinkHtml(html, false, htmlLinkEvaluator);
+		}
+		public static string AutoHyperlinkHtml(string html, bool noFollow, HtmlLinkEvaluator htmlLinkEvaluator) {
+			return FullATagOrTagOrInformalUrlRegex.Replace(html, delegate(Match match) {
+				if(!FullATagOrTagRegex.IsMatch(match.Value)) {
+					return HyperlinkUrl(match.Value, noFollow, htmlLinkEvaluator);
+				} 
+				return match.Value;
 			});
 		}
 		public static string ConvertNewLinesToBr(string text) {
@@ -122,5 +115,38 @@ namespace nJupiter.Web.UI {
 		}
 		#endregion
 
+		#region Helper Methods
+		public static string HyperlinkUrl(string url, bool noFollow, HtmlLinkEvaluator htmlLinkEvaluator) {
+			string linkText = url;
+			string linkUrl;
+			StringBuilder linkAttributes = null;
+			if(InformalUrlPrefixRegex.IsMatch(linkText)) {
+				linkUrl = linkText;
+			} else if(InformalUrlEmailRegex.IsMatch(linkText)) {
+				linkUrl = Uri.UriSchemeMailto + Colon + linkText;
+			} else if(string.Compare(linkText.Substring(0, 3), Uri.UriSchemeFtp, true, CultureInfo.InvariantCulture).Equals(0)) {
+				linkUrl = Uri.UriSchemeFtp + Uri.SchemeDelimiter + linkText;
+			} else {
+				linkUrl = Uri.UriSchemeHttp + Uri.SchemeDelimiter + linkText;
+			}
+			if(htmlLinkEvaluator != null) {
+				HtmlLink htmlLink = htmlLinkEvaluator(new HtmlLink(linkUrl, linkText));
+				linkUrl = htmlLink.Url;
+				linkText = htmlLink.Text;
+				if(!htmlLink.Attributes.Count.Equals(0)) {
+					linkAttributes = new StringBuilder();
+					foreach(string attribute in htmlLink.Attributes.Keys) {
+						linkAttributes.AppendFormat(CultureInfo.InvariantCulture, FormatAttribute, attribute, htmlLink.Attributes[attribute]);
+					}
+				}
+			}
+			return string.Format(CultureInfo.InvariantCulture, 
+				FormatReplaceUrl, 
+				noFollow ? NoFollowAttribute : string.Empty, 
+				linkAttributes,
+				linkUrl, 
+				linkText);
+		}
+		#endregion
 	}
 }
