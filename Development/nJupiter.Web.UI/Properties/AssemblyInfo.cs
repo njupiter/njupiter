d2e8416be3ee5f4b4e3522e4065ae7ac1cc77e1c
martin.odhelius@gmail.com <martin.odhelius@gmail.com@81644cf2-1715-11df-9fa9-695f62f9af20>
2012-04-13 17:00:00 +0000

    Version for nJupiter.Web.UI updated

--- Development/nJupiter.Web.UI/Properties/AssemblyInfo.cs
+++ Development/nJupiter.Web.UI/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 // Increment AssemblyVersion only on major and minor releases
 [assembly: AssemblyVersion("4.0.1.308")]
 // Increment AssemblyFileVersion on every release
-[assembly: AssemblyFileVersion("4.0.1.308")]
+[assembly: AssemblyFileVersion("4.1.0.349")]
