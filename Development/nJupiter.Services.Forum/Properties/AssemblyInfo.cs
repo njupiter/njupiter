69f2914206982c89ea441dc16ec99467056a29f6
martin.odhelius@gmail.com <martin.odhelius@gmail.com@81644cf2-1715-11df-9fa9-695f62f9af20>
2012-05-30 14:52:35 +0000

    Forum libs removed from development folder since it is not updated in years and contain lot of legacy code. Refer to earlier tags if you need the the code.

--- Development/nJupiter.Services.Forum/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-[assembly: AssemblyDescription("Forum API")]
-
-// Increment AssemblyVersion only on major and minor releases
-[assembly: AssemblyVersion("4.0.1.308")]
-// Increment AssemblyFileVersion on every release
-[assembly: AssemblyFileVersion("4.0.1.308")]
