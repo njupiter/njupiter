a94563a78452058a2460cfabb5c7c0418061e3de
martin.odhelius@gmail.com <martin.odhelius@gmail.com@81644cf2-1715-11df-9fa9-695f62f9af20>
2012-09-17 09:11:36 +0000

    Fix NuGet package for nJupiter.DataAccess.Users 4.1.0.505

--- Development/nJupiter.DataAccess.Users.DirectoryService/Properties/AssemblyInfo.cs
+++ Development/nJupiter.DataAccess.Users.DirectoryService/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@
 // Increment AssemblyVersion only on major and minor releases
 [assembly: AssemblyVersion("4.0.2.322")]
 // Increment AssemblyFileVersion on every release
-[assembly: AssemblyFileVersion("4.0.2.322")]
+[assembly: AssemblyFileVersion("4.1.0.505")]
