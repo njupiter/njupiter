3d3fdb1475c1612fde114a44f7faa2b2110ec390
peter.korsback@gmail.com <peter.korsback@gmail.com@81644cf2-1715-11df-9fa9-695f62f9af20>
2013-07-04 14:32:08 +0000

    Added new solution, change log, nuspec and and nuget package for nJupiter.Web as a first step in splitting nJupiter.WebTools into two separate packages.

--- Development/nJupiter.Web/Properties/AssemblyInfo.cs
+++ Development/nJupiter.Web/Properties/AssemblyInfo.cs
@@ -1,12 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyDescription("nJupiter Web Component")]
+[assembly: AssemblyDescription("Contains methods for easier management of query strings, accepted types, mime type objects and permanent redirects.")]
 
 // Increment AssemblyVersion only on major and minor releases
 [assembly: AssemblyVersion("4.0.1.308")]
 // Increment AssemblyFileVersion on every release
-[assembly: AssemblyFileVersion("4.4.5.533")]
+[assembly: AssemblyFileVersion("4.4.6.538")]
+[assembly: AssemblyInformationalVersion("4.4.6.538")]
 
 #if DEBUG
 [assembly: InternalsVisibleTo("nJupiter.Web.Tests.Unit")]
