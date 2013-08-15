15d6d69cfc847b72903d71c5a28c936d7834c931
peter.korsback@gmail.com <peter.korsback@gmail.com@81644cf2-1715-11df-9fa9-695f62f9af20>
2013-08-15 09:19:51 +0000

    Added more descriptive description for nJupiter.Web.UI assembly.

--- Development/nJupiter.Web.UI/Properties/AssemblyInfo.cs
+++ Development/nJupiter.Web.UI/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyDescription("General UI controls")]
+[assembly: AssemblyDescription("Contains Control Adapters for striping ugly content that ASP.NET is generating by default (for example unnecessary id-attributes), also contains a set o web controls that either is not represented in ASP.NET or that extend the functionality of the native controls. The component also have extended functionality for registering scripts to your page and to find controls by type rather than name and also a set of string constants that contains the most commonly used HTML entities.")]
 
 // Increment AssemblyVersion only on major and minor releases
 [assembly: AssemblyVersion("4.0.1.308")]
